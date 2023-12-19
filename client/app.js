8fc37c9571dda87fc1693bd2136de8ef256f47ec
Myles Billignton <mylesbillington@gmail.com>
2023-12-19 15:08:12 +0000

    more changes

--- client/app.js
+++ client/app.js
@@ -1,8 +1,15 @@
-const form = document.getElementById("form");
+const form = document.getElementById("messageForm");
 
 form.addEventListener("submit",(event)=>{
     event.preventDefault();
 
     const formData = new FormData(form);
     const formValues = Object.fromEntries(formData);
+
+    fetch("http://localhost:8080/messages",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body: JSON.stringify(formValues)
 });
+});
+
--- server/server.js
+++ server/server.js
@@ -8,6 +8,13 @@ app.get("/",(req,res)=> {
     response.json('Looking at root route');
 });
 
+app.post("/messages",(req,res)=>{
+    console.log("This is working");
+    const newMessage = request.body;
+    console.log(newMessage);
+    response.json(newMessage);
+})
+
 app.listen(8080,() =>{
     console.log("App is running");
 })
